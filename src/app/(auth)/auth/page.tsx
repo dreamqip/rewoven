93c02272f86a9143e111a88bdfb9238cedf503ef
dreamqip <v.chuchupak@gmail.com>
2024-03-08 01:32:04 +0100

    fix: wrap stepper in a suspense boundary

--- src/app/(auth)/auth/page.tsx
+++ src/app/(auth)/auth/page.tsx
@@ -3,8 +3,22 @@
 import Image from "next/image";
 import { useRouter, useSearchParams } from "next/navigation";
 import { FirstStep, SecondStep, Welcome } from "./components/";
+import { Suspense } from "react";
 
 export default function Page() {
+  return (
+    <>
+      <div className="flex justify-center bg-rewoven-ivory px-4 pt-8">
+        <Image src="/logo.svg" alt="Rewoven Logo" width={93} height={20} />
+      </div>
+      <Suspense>
+        <Stepper />
+      </Suspense>
+    </>
+  );
+}
+
+function Stepper() {
   const searchParams = useSearchParams();
   const { replace } = useRouter();
 
@@ -16,23 +30,12 @@ export default function Page() {
     replace(`/auth?step=${step}`);
   }
 
-  function renderStep() {
-    switch (Number(searchParams.get("step"))) {
-      case 1:
-        return <FirstStep onNext={() => updateStep(2)} />;
-      case 2:
-        return <SecondStep />;
-      default:
-        return <Welcome onNext={() => updateStep(1)} />;
-    }
+  switch (Number(searchParams.get("step"))) {
+    case 1:
+      return <FirstStep onNext={() => updateStep(2)} />;
+    case 2:
+      return <SecondStep />;
+    default:
+      return <Welcome onNext={() => updateStep(1)} />;
   }
-
-  return (
-    <>
-      <div className="flex justify-center bg-rewoven-ivory px-4 pt-8">
-        <Image src="/logo.svg" alt="Rewoven Logo" width={93} height={20} />
-      </div>
-      {renderStep()}
-    </>
-  );
 }
